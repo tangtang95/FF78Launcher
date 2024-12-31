b437b5a9f46cfb162ed7c11527a222757e410853
Tangtang Zhou <tangtang2995@gmail.com>
2024-12-31 13:22:08 +0100

    chore: refactor build.zig and version.rc

--- build.zig
+++ build.zig
@@ -1,23 +1,36 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
+    const app_name = "FF78Launcher";
     const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = .x86, .abi = .msvc } });
     const optimize = b.standardOptimizeOption(.{});
     if (target.result.os.tag != .windows or target.result.abi != .msvc) {
         @panic("build supported only for windows msvc abi");
     }
 
-    const exe = b.addExecutable(.{
-        .name = "FF78Launcher",
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.linkLibC();
-    exe.want_lto = false;
-
     const plog = b.dependency("plog", .{});
     const tomlplusplus = b.dependency("tomlplusplus", .{});
     const stackwalker = b.dependency("StackWalker", .{ .target = target, .optimize = optimize });
+
+    const exe = b.addExecutable(.{
+        .name = app_name,
+        .target = target,
+        .optimize = optimize,
+        .version = .{ .major = 0, .minor = 3, .patch = 0 },
+    });
+    exe.addWin32ResourceFile(.{ .file = b.path("src/version.rc"), .flags = &.{
+        "/dVER_FILEVERSION=0.3.0",
+        "/dVER_FILEVERSION_STR=\"0.3.0\\0\"",
+        "/dVER_PRODUCTNAME=\"" ++ app_name ++ "\"",
+        "/dVER_ORIGINALFILENAME=\"" ++ app_name ++ ".exe\"",
+    } });
+    exe.want_lto = false;
+
+    // for compiling to a different arch than native (e.g. x86 on x86_64 machine), use xwin
+    if(exe.rootModuleTarget().cpu.arch != b.graph.host.result.cpu.arch) {
+        setLibcWithXWin(b, exe);
+    }
+    exe.linkLibC();
 
     exe.addIncludePath(plog.path("include"));
     exe.addIncludePath(tomlplusplus.path("include"));
@@ -30,11 +43,6 @@ pub fn build(b: *std.Build) !void {
     exe.linkSystemLibrary("user32");
     exe.linkSystemLibrary("shell32");
 
-    // for compiling to a different arch than native (e.g. x86 on x86_64 machine), use xwin
-    if(exe.rootModuleTarget().cpu.arch != b.graph.host.result.cpu.arch) {
-        setLibcWithXWin(b, exe);
-    }
-
     exe.addCSourceFiles(.{
         .root = b.path("src"),
         .files = &.{
@@ -46,18 +54,18 @@ pub fn build(b: *std.Build) !void {
             "-std=c++20",
             "-DWIN32",
             "-DWIN32_WINNT=0x0601",
-            "-DAPP_RELEASE_NAME=\"FF78Launcher\"",
-            "-DAPP_RELEASE_VERSION=\"0.0.0\"",
-            "-DAPP_CONFIG_FILE=\"FF78Launcher.toml\"",
+            "-DAPP_RELEASE_NAME=\"" ++ app_name ++ "\"",
+            "-DAPP_RELEASE_VERSION=\"0.3.0\"",
+            "-DAPP_CONFIG_FILE=\"" ++ app_name ++ ".toml\"",
             "-DTOML_ENABLE_SIMD=0", // TODO: temporary disabling SIMD to build
             "-D_CRT_SECURE_NO_WARNINGS", // TODO: remove
             "-DNOMINMAX", // NOTE: is it really needed?
         },
     });
-    exe.addWin32ResourceFile(.{ .file = b.path("src/version.rc") });
+
     b.installArtifact(exe);
     b.installFile("LICENSE", "bin/COPYING.txt");
-    b.installFile("misc/FF78Launcher.toml", "bin/FF78Launcher.toml");
+    b.installFile("misc/FF78Launcher.toml", "bin/" ++ app_name ++ ".toml");
 }
 
 fn setLibcWithXWin(b: *std.Build, exe: *std.Build.Step.Compile) void {
